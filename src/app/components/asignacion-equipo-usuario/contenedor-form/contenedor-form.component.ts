dfde9d278e3aeba760fcc57a126d078e9e47e973
Oscar Hernández <oscar.hernandez@alcegix.mx>
2023-05-11 16:17:09 -0600

    Se integran servicios de contenedores

--- src/app/components/asignacion-equipo-usuario/contenedor-form/contenedor-form.component.ts
+++ src/app/components/asignacion-equipo-usuario/contenedor-form/contenedor-form.component.ts
@@ -1,144 +1,198 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { NgxFileDropEntry } from 'ngx-file-drop';
-import { ArchivoUsuarioInventario, ImagenUsuarioInventarioContenedor, UsuarioInventarioContenedorModel } from 'src/app/modelos/Inventarios/usuario-inventario.model';
-import { FileManagerService } from 'src/app/servicios/filemanager.service';
-import { InventariosService } from 'src/app/servicios/inventarios.service';
-import { SwalServices } from 'src/app/servicios/sweetalert2.services';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { NgxFileDropEntry } from "ngx-file-drop";
+import {
+  ArchivoUsuarioInventario,
+  ImagenUsuarioInventarioContenedor,
+  UsuarioInventarioContenedorModel,
+} from "src/app/modelos/Inventarios/usuario-inventario.model";
+import { FileManagerService } from "src/app/servicios/filemanager.service";
+import { InventariosService } from "src/app/servicios/inventarios.service";
+import { SwalServices } from "src/app/servicios/sweetalert2.services";
 
 @Component({
-  selector: 'vex-contenedor-form',
-  templateUrl: './contenedor-form.component.html',
-  styleUrls: ['./contenedor-form.component.scss']
+  selector: "vex-contenedor-form",
+  templateUrl: "./contenedor-form.component.html",
+  styleUrls: ["./contenedor-form.component.scss"],
 })
 export class ContenedorFormComponent implements OnInit {
   @Input() item: UsuarioInventarioContenedorModel;
   @Output() newItemEvent = new EventEmitter<boolean>();
   public files: NgxFileDropEntry[] = [];
   formContenedores: FormGroup;
-  listaArchivos  = []
+  listaArchivos = [];
   constructor(
     private formBuilder: FormBuilder,
     private filemanagerService: FileManagerService,
     private swalService: SwalServices,
-    private inventariosService: InventariosService,
+    private inventariosService: InventariosService
   ) {
-
     this.iniciarForm();
-
   }
 
   async ngOnInit() {
-/*     if(this.item.id > 0){
+    /*     if(this.item.id > 0){
       this.item = await this.obtenerContenedor();
     } */
 
-    this.inicializarForm()
+    this.inicializarForm();
   }
 
-  public limpiarFormulario(){
+  public limpiarFormulario() {
     this.id.setValue(this.item.id);
     this.relUsuarioInventarioId.setValue(this.item.relUsuarioInventarioId);
     this.contenedor.setValue(this.item.contenedor);
     this.tblUsuarioInventarioContenedorImagenes.clear();
   }
 
-
   public async obtenerContenedor() {
     const respuesta =
-      await this.inventariosService.obtenerContenedoresAsignacionInventario(this.item.id);
-    return respuesta ? respuesta.output : new UsuarioInventarioContenedorModel();
+      await this.inventariosService.obtenerContenedoresAsignacionInventario(
+        this.item.id
+      );
+    return respuesta
+      ? respuesta.output
+      : new UsuarioInventarioContenedorModel();
   }
 
-
-
-  public iniciarForm(){
+  public iniciarForm() {
     this.formContenedores = this.formBuilder.group({
       id: new FormControl(0),
       relUsuarioInventarioId: new FormControl(0),
-      contenedor: new FormControl('', [Validators.required]),
-      tblUsuarioInventarioContenedorImagenes: this.formBuilder.array([])
+      contenedor: new FormControl("", [Validators.required]),
+      tblUsuarioInventarioContenedorImagenes: this.formBuilder.array([]),
     });
   }
 
-  public inicializarForm(){
+  public inicializarForm() {
     this.id.setValue(this.item.id);
     this.relUsuarioInventarioId.setValue(this.item.relUsuarioInventarioId);
     this.contenedor.setValue(this.item.contenedor);
-    this.item.tblUsuarioInventarioContenedorImagenes.forEach((x)=>{
-
+    this.item.tblUsuarioInventarioContenedorImagenes.forEach((x) => {
       const config = this.formBuilder.group({
         id: new FormControl(x.id),
         imagen: new FormControl(x.imagen),
-        url: new FormControl(this.filemanagerService.obtenerURLRutaArchivo(x.imagen)),
-        descripcion: new FormControl(x.descripcion, [Validators.required])
+        url: new FormControl(
+          this.filemanagerService.obtenerURLRutaArchivo(x.imagen)
+        ),
+        descripcion: new FormControl(x.descripcion, [Validators.required]),
       });
 
       this.tblUsuarioInventarioContenedorImagenes.push(config);
-
     });
-
-
   }
 
-  get id() { return this.formContenedores.get('id') }
-  get relUsuarioInventarioId() { return this.formContenedores.get('relUsuarioInventarioId') }
-  get contenedor() { return this.formContenedores.get('contenedor') }
-  get tblUsuarioInventarioContenedorImagenes() { return this.formContenedores.get('tblUsuarioInventarioContenedorImagenes') as FormArray }
+  get id() {
+    return this.formContenedores.get("id");
+  }
+  get relUsuarioInventarioId() {
+    return this.formContenedores.get("relUsuarioInventarioId");
+  }
+  get contenedor() {
+    return this.formContenedores.get("contenedor");
+  }
+  get tblUsuarioInventarioContenedorImagenes() {
+    return this.formContenedores.get(
+      "tblUsuarioInventarioContenedorImagenes"
+    ) as FormArray;
+  }
 
-  async agregarImagen(token ){
+  async agregarImagen(token) {
     const img = this.formBuilder.group({
       id: new FormControl(0),
       imagen: new FormControl(token),
-      url: new FormControl(this.filemanagerService.obtenerURLRutaArchivo(token)),
-      descripcion: new FormControl('', [Validators.required])
-      });
-      this.tblUsuarioInventarioContenedorImagenes.push(img);
+      url: new FormControl(
+        this.filemanagerService.obtenerURLRutaArchivo(token)
+      ),
+      descripcion: new FormControl("", [Validators.required]),
+    });
+    this.tblUsuarioInventarioContenedorImagenes.push(img);
   }
 
-
-  public async guardarContenedor(){
-    debugger
+  public async guardarContenedor() {
+    debugger;
     const objeto = this.formContenedores.value;
 
-    const respuesta =  this.item.id > 0 ? await this.inventariosService.editarContenedorAsignacionInventario(this.formContenedores.value) : await this.inventariosService.insertaContenedorAsignacionInventario(this.formContenedores.value);
+    const respuesta =
+      this.item.id > 0
+        ? await this.inventariosService.editarContenedorAsignacionInventario(
+            this.formContenedores.value
+          )
+        : await this.inventariosService.insertaContenedorAsignacionInventario(
+            this.formContenedores.value
+          );
 
-    if(respuesta.exito){
-      this.swalService.alertaPersonalizada(true, 'Exito');
+    if (respuesta.exito) {
+      this.swalService.alertaPersonalizada(true, "Exito");
       this.limpiarFormulario();
       this.inicializarForm();
-      this.newItemEvent.emit(true);
-/*       this.item.id = respuesta.id;
+      this.newItemEvent.emit(respuesta.id);
+      /*       this.item.id = respuesta.id;
       this.ngOnInit() */
     } else {
-      this.swalService.alertaPersonalizada(false, 'Error');
+      this.swalService.alertaPersonalizada(false, "Error");
     }
-
   }
 
-  public async eliminarArchivo(i, archivo){//archivo: ImagenUsuarioInventarioContenedor){
-debugger
+  public async eliminarArchivo(i, archivo) {
+    //archivo: ImagenUsuarioInventarioContenedor){
+    debugger;
 
-    let a = archivo;
-    let confirmacion = await this.swalService.confirmacion("Atención","¿Esta seguro de eliminar el registro?", "Eliminar","");
-    if(confirmacion){
-      const respuesta = await this.inventariosService.eliminarImagenContenedorAsignacionInventario(archivo);
-      if(respuesta.exito){
-        this.swalService.alertaPersonalizada(true, 'Exito');
-        this.tblUsuarioInventarioContenedorImagenes.removeAt(i)
-        //this.newItemEvent.emit(true);
-        //this.recargar();
-      } else {
-        this.swalService.alertaPersonalizada(false, 'Error');
+    if (archivo.value.id > 0) {
+      let a = i;
+      let b = archivo.value;
+      let confirmacion = await this.swalService.confirmacion(
+        "Atención",
+        "¿Esta seguro de eliminar el registro?",
+        "Eliminar",
+        ""
+      );
+      if (confirmacion) {
+        const respuesta =
+          await this.inventariosService.eliminarImagenContenedorAsignacionInventario(
+            archivo.value.id
+          );
+        if (respuesta.exito) {
+          this.swalService.alertaPersonalizada(true, "Exito");
+          this.tblUsuarioInventarioContenedorImagenes.removeAt(i);
+
+          //this.newItemEvent.emit(true);
+          //this.recargar();
+        } else {
+          this.swalService.alertaPersonalizada(false, "Error");
+        }
       }
+    } else {
+      this.tblUsuarioInventarioContenedorImagenes.removeAt(i);
     }
   }
 
-  async abrirDocumento(token: string){
-    let url = await this.filemanagerService.obtenerRutaArchivo(token);
-    window.open(url,'_blank');
-   }
+  public async recargar() {
+    this.tblUsuarioInventarioContenedorImagenes.clear();
+    this.item.tblUsuarioInventarioContenedorImagenes.forEach((x) => {
+      const config = this.formBuilder.group({
+        id: new FormControl(x.id),
+        imagen: new FormControl(x.imagen),
+        url: new FormControl(
+          this.filemanagerService.obtenerURLRutaArchivo(x.imagen)
+        ),
+        descripcion: new FormControl(x.descripcion, [Validators.required]),
+      });
 
+      this.tblUsuarioInventarioContenedorImagenes.push(config);
+    });
+  }
+
+  async abrirDocumento(token: string) {
+    let url = await this.filemanagerService.obtenerRutaArchivo(token);
+    window.open(url, "_blank");
+  }
 
   public dropped(files: NgxFileDropEntry[]) {
     this.files = files;
